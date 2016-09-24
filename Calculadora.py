76048d4dc377e8fe75ee170fc9fb79b6f58ec98c
vilm89 <riguin89@gmail.com>
2016-09-24 10:36:13 -0500

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,69 @@
+import os
+
+while (opc<=5):
+
+print("Calculadora")
+
+print("Menu")
+
+Print("---------------")
+
+print("1) Suma")
+
+print("2) Resta")
+
+print("3) Multiplicacion")
+
+print("4) Division")
+
+print("5) Salir")
+
+opc=input("elige una opcion:")
+
+x=int(input("ingresa un numero:"))
+y=int(input("ingresa otro numero:"))
+
+    if opc==1 and opc==2 and opc==3 and opc==4 and opc==<5:
+
+        suma = x+y
+
+        print("la suma es:", suma)
+
+        os.system("cls")
+ 
+        opc=input("elige una opcion:")
+
+
+    if opc==1 and opc==2 and opc==3 and opc==4 and opc==<5:
+
+        resta = x-y
+ 
+        print("la resta es:", resta)
+
+        os.system("cls")
+
+        opc=input("elige una opcion:")
+
+    if opc==1 and opc==2 and opc==3 and opc==4 and opc==<5:
+
+        multiplicacion = x*y
+
+        print("la multiplicacion es:", multiplicacion)
+
+        os.system("cls")
+
+        opc=input("elige una opcion:")
+
+    if opc==1 and opc==2 and opc==3 and opc==4 and opc==<5:
+
+        division = x/y
+
+        print("la division es:", division)
+
+        os.system("cls")
+
+        opc=input("elige una opcion:")
+
+    if opc==5:
+
+        break
